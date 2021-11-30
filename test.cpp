410c906fb5d88bf80ca1898c8d5e008e3f890d54
kotchen <549642010@qq.com>
2021-11-30 14:22:15 +0800

    测试内存模型

--- test.cpp
+++ test.cpp
@@ -6,8 +6,15 @@ const char* sstr = "hello world!";
 
 int main()
 {
-    auto p = str;
-    auto pp = sstr;
-    printf("%p %p", str, sstr);
+    const char* p = str;
+    const char* pp = sstr;
+    int a  = 0;
+    int b  = 2;
+    int c = 1;
+    int d = 1;
+    printf("%p %p\n", &p, &pp);
+    
+    printf("%p %p\n", &a, &b);
+    printf("%p %p", &c, &d);
     std::cin.get();
 }
