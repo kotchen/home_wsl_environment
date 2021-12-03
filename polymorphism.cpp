dbf0f1581e71d2366e68a226b3607e5b216a4d99
kotchen <549642010@qq.com>
2021-12-03 15:19:16 +0800

    准备修改项目结构

---
+++ polymorphism.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+
+class Ispeaker
+{
+protected:
+    size_t b;
+public:
+    Ispeaker(size_t _v) 
+        : b(_v)
+    {}
+    virtual void speak() = 0;
+};
+
+class Dog : Ispeaker
+{
+private:
+    int feet;
+public:
+    Dog() 
+        : Ispeaker(0), feet(4)
+    {}
+
+    virtual void eat_shit()
+    {
+        printf("eat shit\n");
+    }
+
+    virtual void speak()
+    {
+        printf("dog! %lu\n", b);
+    }
+};
+
+class Shiba : Dog
+{
+public:
+    Shiba()
+        : Dog()
+    {}
+    
+    // virtual void speak()
+    // {
+    //     printf("Shiba\n");
+    // }
+};
+
+class Human : Ispeaker
+{
+private:
+    size_t c;
+public:
+    Human()
+        : Ispeaker(1),
+          c(2)
+    {}
+
+    virtual void speak()
+    {
+        printf("human! %lu\n", c);
+    }
+};
+
+int main()
+{
+    Dog* speaker_1 = new Dog();
+    Human* speaker_2 = new Human();
+    Shiba* speaker_3 = new Shiba();
+
+    Ispeaker* p1 = (Ispeaker*)speaker_1;
+    Ispeaker* p2 = (Ispeaker*)speaker_2;
+    Ispeaker* p3 = (Ispeaker*)speaker_3;
+
+    p1->speak();
+    p2->speak();
+    p3->speak();
+
+    Dog* p_dog = (Dog*)speaker_3;
+    p1->Ispeaker::speak();
+
+    auto x = malloc(1);
+    delete x;
+    
+}
---
+++ smart_pointer.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+#include <memory>
+
+template<typename T>
+class share_ptr
+{
+public:
+    int* counter;
+    int* weak_ref;
+    T*   data;
+public:
+    // ptr = shared_ptr(e);
+    // ptr = shared_ptr();
+
+    share_ptr(T* resourse = nullptr)
+    {
+        data = resourse;
+        counter = new int(1);
+        weak_ref = new int(0);
+    }
+
+    share_ptr(share_ptr& rhs)
+    {
+        counter = rhs.counter;
+        data = rhs.data;
+        ++*counter;
+    }
+
+    share_ptr& operator= (share_ptr& rhs)
+    {
+        --*counter;
+        if (*counter==0)
+        {
+            delete data;
+            delete counter;
+            delete weak_ref;
+        }
+        data = rhs.data;
+        counter = rhs.counter;
+        ++*counter;
+        return *this;
+    }
+
+    ~share_ptr()
+    {
+        --*counter;
+        if (( *counter ) == 0)
+        {
+            delete counter;
+            delete data;
+            delete weak_ref;
+        }
+    }
+};
+
+
+template<typename T>
+class weak_ptr
+{
+public:
+    T* data;
+public:
+    weak_ptr(T* resource=nullptr):data(resource)
+    {
+    }
+    
+    weak_ptr(share_ptr<T> p)
+    {
+        data = p.data;
+    }
+
+    weak_ptr& operator= (share_ptr<T> p)
+    {
+        data = p.data;
+        *(p.weak_ref)++;
+        return *this;
+    }
+};
+
+struct Entity
+{
+    double x;
+    double y;
+    Entity() : x(0), y(0) {}
+    void move_right(double offset)
+    {
+        x+=offset;
+    }
+};
+
+void bar(std::unique_ptr<Entity> ptr)
+{
+    ptr->move_right(5);
+}
+
+void foo()
+{
+    auto e = std::make_unique<Entity>();
+    e->move_right(2);
+    bar(std::move(e));    
+}
+
+// std::unique_ptr
+// auto e = std::make_unique<>();
+class Son;
+
+class Father {
+public:
+    share_ptr<Son> son_;
+    Father() {
+        std::cout << __FUNCTION__ << std::endl;
+    }
+    ~Father() {
+        std::cout << __FUNCTION__ << std::endl;
+    }
+};
+
+class Son {
+public:
+    weak_ptr<Father> father_;
+    Son() {
+        std::cout << __FUNCTION__ << std::endl;
+    }
+    ~Son() {
+        std::cout << __FUNCTION__ << std::endl;
+    }
+};
+
+
+
+int main(int argc, char const *argv[])
+{
+    // auto son = std::make_shared<Son>();
+    // auto father = std::make_shared<Father>();
+    // son->father_ = father;
+    // father->son_ = son;
+    // std::cout << "son: " << son.use_count() << std::endl;
+    // std::cout << "father: " << father.use_count() << std::endl;
+
+    // Son* son = new Son();
+    // Father* father = new Father();
+
+    // share_ptr<Son> s(son);
+    // share_ptr<Father> f(father); 
+    // s.data->father_ = f;
+    // f.data->son_ = s; 
+    auto son_ = new Son();  // 创建一个Son对象，返回指向Son对象的指针son_
+    auto father_ = new Father();  // 创建一个Father对象，返回指向Father对象的指针father_
+    share_ptr<Son> son(son_);  // 调用SharedPtr构造函数：son.counter=1, son.weakref=0
+    share_ptr<Father> father(father_);  // 调用SharedPtr构造函数：father.counter=1, father.weakref=0
+    son.data->father_ = father;  // 调用WeakPtr赋值函数：father.counter=1, father.weakref=1
+    father.data->son_ = son;  // 调用SharedPtr赋值函数：son.counter=2, son.weakref=0
+
+    return 0;
+}
