511308156d1eaba98b255540d763ae045cc04f6d
kotchen <549642010@qq.com>
2021-12-03 20:44:46 +0800

    调整结构

---
+++ include/binary_tree/tree_node.h
@@ -0,0 +1,9 @@
+﻿struct tree_node
+{
+    int val;
+    tree_node* left;
+    tree_node* right;
+    tree_node():val(0), left(nullptr), right(nullptr){}
+    tree_node(int value): val(value), left(nullptr), right(nullptr){}
+    tree_node(int value, tree_node* _left, tree_node* _right):val(value),left(_left), right(_right){}
+};
