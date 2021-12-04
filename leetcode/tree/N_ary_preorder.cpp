7e6099599d68a3e78996df284a725e02f260bd4e
kotchen <549642010@qq.com>
2021-12-04 22:49:22 +0800

    N叉数的前序遍历

---
+++ leetcode/tree/N_ary_preorder.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include "include/tree_node/Node.h"
+class Solution {
+public:
+
+    std::vector<int> v;
+    void _preorder(Node* root)
+    {
+        if (!root) return ;
+        v.emplace_back(root->val);
+        for (auto i : root->children)
+            _preorder(i);
+    }
+
+    std::vector<int> preorder(Node* root) {
+        _preorder(root);
+        return v;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Node node(1);
+    return 0;
+}
