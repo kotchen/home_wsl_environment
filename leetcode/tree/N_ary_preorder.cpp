01dc017727bc92992ebad20a94b26e9402f94479
kotchen <549642010@qq.com>
2021-12-05 07:13:25 +0800

    N叉树的前序遍历

--- leetcode/tree/N_ary_preorder.cpp
+++ leetcode/tree/N_ary_preorder.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
+#include <stack>
 #include "include/tree_node/Node.h"
 class Solution {
 public:
@@ -18,7 +19,27 @@ public:
         return v;
     }
 };
-
+class Solution {
+public:
+    std::vector<int> preorder(Node* root) {
+        if(!root) return std::vector<int>();
+        std::vector<int> v;
+        std::stack<Node*> s;
+        s.push(root);
+        while (!s.empty())
+        {
+            auto temp = s.top()->children;
+            v.emplace_back(s.top()->val);
+            s.pop();
+            for (auto i = temp.rbegin(); i!=temp.rend(); i++)
+            {
+                if(!(*i)) continue;
+                s.push(*i);
+            }
+        }
+        return v;
+    }
+};
 int main(int argc, char const *argv[])
 {
     Node node(1);
