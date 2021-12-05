a0aa29367c22fda638b8a2ea81bbfb225bddedbf
kotchen <549642010@qq.com>
2021-12-05 10:36:37 +0800

    最深层叶子之和

---
+++ leetcode/tree/deepest_leaves_sum.cpp
@@ -0,0 +1,30 @@
+﻿#include "include/tree_node/binary_tree_node.h"
+#include <map>
+#include <vector>
+class Solution {
+public:
+    std::map<int, std::vector<int>> m;
+    void deep_order(TreeNode* root, int y)
+    {
+        if (!root) return ;
+        if (root->left) deep_order(root->left, y+1);
+        m[y].emplace_back(root->val);
+        if (root->right) deep_order(root->right,y+1);
+    }
+    int deepestLeavesSum(TreeNode* root) {
+        if (!root) return 0;
+        deep_order(root, 0);
+        int res = 0;
+        auto layer = m.rbegin();
+        for (auto i : layer->second)
+            res += i;
+
+        return res;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    /* code */
+    TreeNode node;
+    return 0;
+}
