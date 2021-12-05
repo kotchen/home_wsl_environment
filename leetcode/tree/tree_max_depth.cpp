4d71d180e28b6821bd1579fce15fe363d11ae424
kotchen <549642010@qq.com>
2021-12-05 15:41:06 +0800

    二叉树的最大深度

---
+++ leetcode/tree/tree_max_depth.cpp
@@ -0,0 +1,15 @@
+﻿#include "include/tree_node/binary_tree_node.h"
+#include <iostream>
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        return std::max(maxDepth(root->left),maxDepth(root->right))+1;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
