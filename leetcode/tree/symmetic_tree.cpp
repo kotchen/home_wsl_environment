92d6de64d38791af4b5e0808ba725ea373f53f6d
kotchen <549642010@qq.com>
2021-12-05 15:13:14 +0800

    判断二叉树是否对称

---
+++ leetcode/tree/symmetic_tree.cpp
@@ -0,0 +1,31 @@
+﻿#include "include/tree_node/binary_tree_node.h"
+
+// 舒服了，第一次流畅的写出递归算法
+class Solution {
+public:
+
+    bool helper(TreeNode* left, TreeNode* right)
+    {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+        if(left->val == right->val)  
+        {
+            return helper(left->left, right->right) &&
+                   helper(left->right, right->left);
+        }
+        else return false;
+    }
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+
+        return helper(root->left, root->right);
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+
+    TreeNode node;
+    return 0;
+}
