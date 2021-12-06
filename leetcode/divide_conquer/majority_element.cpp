911f489799c0c2d4d5b75e0f485c58102d701e06
kotchen <549642010@qq.com>
2021-12-06 19:56:10 +0800

    数组中最多的数（超过半数）

---
+++ leetcode/divide_conquer/majority_element.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+// 自相残杀
+class Solution {
+public:
+    int majorityElement(std::vector<int>& nums) {
+       int count = 0;
+       int res = nums[0];
+       for (auto i : nums)
+       {
+           if (i==res)
+            {
+                count++;
+                i = res;
+            }
+           else count--;
+           if (count==-1)
+           {
+               count = 1;
+               res = i;
+           }
+       }
+       return res;
+    }
+};
+// class Solution {
+// public:
+//     int majorityElement(std::vector<int>& nums) {
+//         std::sort(nums.begin(), nums.end());
+//         return nums[nums.size()/2];
+//     }
+// };
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
