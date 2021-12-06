bfde5ae05f7f89a2c6b5e5a864b34a87dab42580
kotchen <549642010@qq.com>
2021-12-06 21:47:23 +0800

    旋转排序数组中最小的值

---
+++ leetcode/divide_conquer/min_in_rotation_array.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int findMin(std::vector<int>& nums) {
+        int res = nums[0];
+        for (int i=0;i+1<nums.size();i++)
+        {
+            if (nums[i]>nums[i+1]) return nums[i+1];
+        }
+        return res;
+    }
+};
+
+// class Solution {
+// public:
+
+//     int helper(vector<int>& nums, int left, int right)
+//     {
+//         if (left >= right) return std::min(nums[left],nums[right]);
+//         int mid = (left+right)/2;
+//         return std::min(helper(nums,left,mid),
+//                         helper(nums, mid+1, right));
+//     }
+
+//     int findMin(vector<int>& nums) {
+//         return helper(nums,0,nums.size()-1);
+//     }
+// };
+
+// class Solution {
+// public:
+
+//     int helper(vector<int>& nums, int left, int right)
+//     {
+//         if (nums[left]<=nums[right])
+//         {
+//             return nums[left];
+//         }
+//         int mid = (left+right)/2;
+//         return std::min(helper(nums, left, mid),
+//                         helper(nums, mid+1, right));
+//     }
+
+//     int findMin(vector<int>& nums) {
+//         return helper(nums,0,nums.size()-1);
+//     }
+// };
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
