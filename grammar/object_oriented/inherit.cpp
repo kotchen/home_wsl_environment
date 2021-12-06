f9ff2998be750214063749c3262cd068834b675d
kotchen <549642010@qq.com>
2021-12-06 19:47:04 +0800

    增加多继承

--- grammar/object_oriented/inherit.cpp
+++ grammar/object_oriented/inherit.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
-
+﻿#include <iostream>
 class Base
 {
 public: 
-    int a;
+    int32_t a;
     char* b;
 
     Base() : a(0), b(nullptr) {};
@@ -36,13 +35,57 @@ public:
         
     }
 };
-int main ()   
-{   
-   Base* pa = new Base();
-   Derived* pb = new Derived();
-   pa->qwe(0);
-   pb->qwe(0);
-   delete pa;
-   delete pb;
-   Base b;
+class IRunner {
+private:
+	size_t a;
+public:
+	IRunner()
+		: a(0){
+	}
+	virtual void run() = 0;
+};
+
+class ISpeaker{
+protected:
+	size_t b;
+public:
+	ISpeaker( size_t _v )
+		: b(_v) 
+	{}
+	virtual void speak() = 0;
+};
+
+class Dog : public ISpeaker {
+public:
+	Dog()
+		: ISpeaker(1)
+	{}
+	//
+	virtual void speak() override {
+		printf("woof! %lu\n", b);
+	}
+};
+
+class RunnerDog : public IRunner, public Dog {
+public:
+	RunnerDog()
+	{}
+
+    virtual void speak()
+    {
+        printf("RunnerDog\n");
+    }
+
+	virtual void run() override {
+		printf("run with 4 legs\n");
+	}
+};
+
+
+int main(int argc, char const *argv[])
+{
+    auto pdog = new RunnerDog();
+    ISpeaker* speaker = pdog;
+    speaker->speak();
+    return 0;
 }
