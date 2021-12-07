b4b04ccb616f667b3ba3366562ca2f59497d0d68
kotchen <549642010@qq.com>
2021-12-07 08:28:28 +0800

    旋转数组中的最小值

--- leetcode/divide_conquer/min_in_rotation_array.cpp
+++ leetcode/divide_conquer/min_in_rotation_array.cpp
@@ -1,33 +1,33 @@
 ﻿#include <iostream>
 #include <vector>
 using namespace std;
-class Solution {
-public:
-    int findMin(std::vector<int>& nums) {
-        int res = nums[0];
-        for (int i=0;i+1<nums.size();i++)
-        {
-            if (nums[i]>nums[i+1]) return nums[i+1];
-        }
-        return res;
-    }
-};
-
 // class Solution {
 // public:
-
-//     int helper(vector<int>& nums, int left, int right)
-//     {
-//         if (left >= right) return std::min(nums[left],nums[right]);
-//         int mid = (left+right)/2;
-//         return std::min(helper(nums,left,mid),
-//                         helper(nums, mid+1, right));
-//     }
-
-//     int findMin(vector<int>& nums) {
-//         return helper(nums,0,nums.size()-1);
+//     int findMin(std::vector<int>& nums) {
+//         int res = nums[0];
+//         for (int i=0;i+1<nums.size();i++)
+//         {
+//             if (nums[i]>nums[i+1]) return nums[i+1];
+//         }
+//         return res;
 //     }
 // };
+//  有相同元素
+class Solution {
+public:
+
+    int helper(vector<int>& nums, int left, int right)
+    {
+        if (left >= right) return std::min(nums[left],nums[right]);
+        int mid = (left+right)/2;
+        return std::min(helper(nums,left,mid),
+                        helper(nums, mid+1, right));
+    }
+
+    int findMin(vector<int>& nums) {
+        return helper(nums,0,nums.size()-1);
+    }
+};
 
 // class Solution {
 // public:
@@ -50,6 +50,8 @@ public:
 
 int main(int argc, char const *argv[])
 {
-    
+    Solution s;
+    vector<int> v = {3,1,3};
+    std::cout << s.findMin(v) << '\n';
     return 0;
 }
