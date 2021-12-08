49524ce6f5b7890143cd892fd6a2baf132bdcafe
kotchen <549642010@qq.com>
2021-12-09 07:21:52 +0800

    右侧比该数更小的数的个数

---
+++ leetcode/divide_conquer/count_small_after_self.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        vector<int> res;
+       for (auto i = nums.begin(); i!=nums.end(); i++) 
+       {
+            int count = 0;
+           vector<int> temp(i, nums.end());
+           sort(temp.begin(),temp.end());
+           for (int j = 0; j<temp.size(); j++)
+           {
+               if (*i>temp[j]) count++;
+               else break;
+           }
+           res.push_back(count);
+       }
+       return res;
+  
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution s;
+    vector<int> v = {2,0,1};
+    for (auto i : s.countSmaller(v))
+        std::cout << i << ' ';
+    std::cout << '\n';
+    return 0;
+}
