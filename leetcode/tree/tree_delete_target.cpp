987c2ed3737cdfda74e7a0dc0370acb73bb4ab2d
kotchen <549642010@qq.com>
2021-12-11 11:43:29 +0800

    删除给定值的叶子节点

---
+++ leetcode/tree/tree_delete_target.cpp
@@ -0,0 +1,23 @@
+﻿#include "include/tree_node/binary_tree_node.h"
+class Solution {
+public:
+
+    TreeNode* trim(TreeNode* root, int target)
+    {
+        if (!root) return nullptr;
+        root->left = trim(root->left, target);
+        root->right = trim(root->right, target);
+        if (root->val == target && !root->left && !root->right) 
+            return nullptr;
+        return root;
+    }
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        return trim(root, target);
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
