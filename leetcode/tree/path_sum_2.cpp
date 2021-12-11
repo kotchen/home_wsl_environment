e5708ff58451f624c71d3d6d942ee12a639c9669
kotchen <549642010@qq.com>
2021-12-11 15:48:34 +0800

    求出所有总和为target的路径

---
+++ leetcode/tree/path_sum_2.cpp
@@ -0,0 +1,38 @@
+﻿#include "include/tree_node/binary_tree_node.h"
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> res;
+    void helper(TreeNode* root, int targetSum, vector<int>& v)
+    {
+        if (!root) return ;
+        if (!root->left && !root->right)
+        {
+            if (root->val == targetSum)
+            {
+                v.push_back(root->val);
+                res.push_back(v);
+                v.pop_back();
+            }
+            return ;
+        }
+        v.push_back(root->val);
+        helper(root->left, targetSum-root->val, v);
+        helper(root->right, targetSum-root->val, v);
+        v.pop_back();
+    }
+
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<int> v;
+        helper(root, targetSum, v);
+        return res;
+    }
+};
+ 
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
