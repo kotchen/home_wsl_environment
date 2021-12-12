258dd2b9030acda10633f34f7436b37a764fb7df
kotchen <549642010@qq.com>
2021-12-12 14:13:33 +0800

    大写字母转小写字母

---
+++ leetcode/str/to_low_case.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+using namespace std;
+class Solution {
+public:
+    int offset = 'a' - 'A';
+    string toLowerCase(string s) {
+        for (int i = 0; i<s.size(); i++)
+        {
+            if (s[i]>='A'&& s[i]<='Z')
+                s[i] = s[i] + offset;
+        }
+        return s;
+    }
+};
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
