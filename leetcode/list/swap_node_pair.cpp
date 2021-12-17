a65c0a4f1f1d15ca9807aa4d642b441dc397ee8c
kotchen <549642010@qq.com>
2021-12-17 10:14:58 +0800

    链表节点两两交换

---
+++ leetcode/list/swap_node_pair.cpp
@@ -0,0 +1,32 @@
+﻿#include "include/tree_node/list_node.h"
+class Solution {
+public:
+    void helper(ListNode* left, ListNode* right, ListNode* pre)
+    {
+        if (!right) 
+        {
+            pre->next = left;
+            return ;
+        }
+        auto temp = right->next;
+        right->next = left;
+        left->next = temp;
+        if (pre) pre->next = right;
+        pre = left;
+        if (pre->next) helper(pre->next, pre->next->next, pre);
+        else pre->next = nullptr;
+    }
+
+    ListNode* swapPairs(ListNode* head) {
+        if (!head) return nullptr;
+        if (!head->next) return head;
+        auto res = head->next;
+        helper(head, head->next, nullptr);
+        return res;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
