fb1685a7ca09868661d1229423b16051c6529898
kotchen <549642010@qq.com>
2021-12-17 08:32:01 +0800

    两数相加

---
+++ include/tree_node/list_node.h
@@ -0,0 +1,10 @@
+﻿#ifndef __LIST_NODE__
+#define __LIST_NODE__
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+#endif
---
+++ leetcode/list/add_two_nums.cpp
@@ -0,0 +1,54 @@
+﻿#include "include/tree_node/list_node.h"
+class Solution {
+public:
+    ListNode* res;
+    void helper(ListNode* l1, ListNode* l2, ListNode* cur, int car)
+    {
+        if (!l1 && !l2)
+        {
+            if (car) 
+            {
+                ListNode* temp = new ListNode(1);
+                cur->next = temp;
+            }
+            return ;
+        }
+        if (l1 && !l2)
+        {
+            int value = (l1->val+car)%10;
+            car = (l1->val+car)/10;
+            ListNode* temp = new ListNode(value);
+            cur->next = temp;
+            cur = cur->next;
+            helper(l1->next, l2, cur, car);
+        }
+        if (!l1 && l2)
+        {
+            int value = (l2->val+car)%10;
+            car = (l2->val+car)/10;
+            ListNode* temp = new ListNode(value);
+            cur->next = temp;
+            cur = cur->next;
+            helper(l1, l2->next, cur, car);
+        }
+        if (l1 && l2)
+        {
+            int value = (l1->val+l2->val+car)%10;
+            car = (l1->val+l2->val+car)/10;
+            ListNode* temp = new ListNode(value);
+            cur->next = temp;
+            cur = cur->next;
+            helper(l1->next, l2->next, cur, car);
+        }
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        res = new ListNode(0);
+        helper(l1, l2, res, 0);
+        return res->next;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
