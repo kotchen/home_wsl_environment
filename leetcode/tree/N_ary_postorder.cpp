bf88c2c1ec911afd6c1a6a330e3499e835376636
kotchen <549642010@qq.com>
2021-12-17 20:47:41 +0800

    N叉树的后序遍历

--- leetcode/tree/N_ary_postorder.cpp
+++ leetcode/tree/N_ary_postorder.cpp
@@ -3,25 +3,25 @@
 #include <stack>
 #include "include/tree_node/Node.h"
 #include <algorithm>
-class Solution {
-public:
-    std::vector<int> postorder(Node* root) {
-        if (!root) return std::vector<int>();
-        std::stack<Node*> s;
-        std::vector<int> v;
-        s.push(root);
-        while (!s.empty())
-        {
-            auto temp = s.top();
-            v.emplace_back(temp->val);
-            s.pop();
-            for (auto i : temp->children)
-                s.push(i);
-        }
-        reverse(v.begin(), v.end());
-        return v;
-    }
-};
+// class Solution {
+// public:
+//     std::vector<int> postorder(Node* root) {
+//         if (!root) return std::vector<int>();
+//         std::stack<Node*> s;
+//         std::vector<int> v;
+//         s.push(root);
+//         while (!s.empty())
+//         {
+//             auto temp = s.top();
+//             v.emplace_back(temp->val);
+//             s.pop();
+//             for (auto i : temp->children)
+//                 s.push(i);
+//         }
+//         reverse(v.begin(), v.end());
+//         return v;
+//     }
+// };
 
 class Solution {
 public:
