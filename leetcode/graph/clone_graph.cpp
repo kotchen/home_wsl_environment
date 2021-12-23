74cf7c2ed9633f4551255ca3c9947e2cb4593fe3
kotchen <549642010@qq.com>
2021-12-23 14:41:28 +0800

    克隆一个无向图

---
+++ leetcode/graph/clone_graph.cpp
@@ -0,0 +1,29 @@
+﻿#include "include/graph_node/node.h"
+#include <map>
+using namespace std;
+class Solution {
+public:
+    map<int, Node*> m;
+    Node* helper(Node* node)
+    {
+        auto ite = m.find(node->val);
+        if (ite!=m.end())
+            return ite->second;
+        auto node_new = new Node(node->val);
+        m[node_new->val] = node_new;
+        for (auto i : node->neighbors)
+        {
+            node_new->neighbors.push_back(helper(i));
+        }
+        return node_new;
+    }
+    Node* cloneGraph(Node* node) {
+        if (!node)
+            return node;
+        return helper(node);        
+    }
+};
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
