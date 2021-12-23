8d015bc0a10e2fce2cc97160430654f4de85e2ba
kotchen <549642010@qq.com>
2021-12-23 14:43:08 +0800

    克隆无向图

--- leetcode/graph/clone_graph.cpp
+++ leetcode/graph/clone_graph.cpp
@@ -1,6 +1,11 @@
 ﻿#include "include/graph_node/node.h"
 #include <map>
 using namespace std;
+/*
+这里的思路不难，就是遍历所有节点，我这里的写法是dfs
+这里还使用一个map来记录已经构造的节点，如果节点已经构造那么直接返回
+否则就要先构造节点了。
+*/
 class Solution {
 public:
     map<int, Node*> m;
