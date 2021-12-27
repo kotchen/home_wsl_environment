b9c73e97b5e411f53a435265253f7a0cb032a078
kotchen <549642010@qq.com>
2021-12-27 19:01:11 +0800

    最大和的子数组

---
+++ leetcode/dp/subarray_max_sum.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> dp;
+    int maxSubArray(vector<int>& nums) {
+        dp = nums;
+        int res = nums[0];
+        for (int i = 1; i<dp.size(); i++)
+        {
+            dp[i] = max(dp[i-1]+dp[i], dp[i]);
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
