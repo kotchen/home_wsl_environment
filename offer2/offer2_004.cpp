cecf9b0dd07a70e4e74543d19a0f9aabbc60c455
kotchen <549642010@qq.com>
2022-02-21 10:18:27 +0800

    数组中只出现一次的数

--- offer2/offer2_004.cpp
+++ offer2/offer2_004.cpp
@@ -2,6 +2,11 @@
 #include <unordered_map>
 #include <vector>
 using namespace std;
+
+// 一个数组中只出现一次的数，其他的数出现3次
+
+// 第一种方法就是直接hashmap搞统计
+
 class Solution {
 public:
     std::unordered_map<int, int> hash_map;
@@ -23,10 +28,39 @@ public:
     }
 };
 
+//第二种方法就是 比如 数组为 3 3 3 1
+// 3   0011
+// 3   0011
+// 3   0011
+// 1   0001
+
+// 我们用一个32位的数
+class Solution_2
+{
+public:
+    int singleNumber(vector<int>& nums)
+    {
+        int res = 0;
+        for (int i = 0; i<32; ++i)
+        {
+            int count = 0;
+            for (int num : nums)
+            {
+                count += (num>>i)&1;
+            }
+            if (count%3)
+            {
+                res |= (1<<i);
+            }
+        }
+        return res;
+    }
+};
+
 int main(int argc, char const *argv[])
 {
     vector<int> v{1,1,1,3,2,2,2};
-    Solution s;
+    Solution_2 s;
     cout << s.singleNumber(v) << endl;
     return 0;
 }
