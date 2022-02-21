38db51a199cfbeb1860692e5ba019743625a1821
kotchen <549642010@qq.com>
2022-02-21 10:07:24 +0800

    新增offer2

---
+++ offer2/offer2_004.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    std::unordered_map<int, int> hash_map;
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for (auto& i : nums)
+        {
+            ++hash_map[i];
+        }
+        for (auto& [num, count] : hash_map)
+        {
+            if (count == 1)
+            {
+                res = num;
+                break;
+            }
+        }
+        return res;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    vector<int> v{1,1,1,3,2,2,2};
+    Solution s;
+    cout << s.singleNumber(v) << endl;
+    return 0;
+}
